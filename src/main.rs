30927255e285d3a0dfd5003e1bd0b96b8bdd0e63
Alexander Jackson <alexanderjackson@protonmail.com>
2024-03-31 15:53:34 +0100

    feat: add basic lambda event handler

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use lambda_runtime::{service_fn, LambdaEvent};
+
+type LambdaResult<T> = std::result::Result<T, lambda_runtime::Error>;
+
+async fn handler(event: LambdaEvent<()>) -> LambdaResult<()> {
+    tracing::info!(payload = ?event.payload, "Received an event for the lambda invocation!");
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> LambdaResult<()> {
+    tracing_subscriber::fmt().init();
+
+    lambda_runtime::run(service_fn(handler)).await?;
+
+    Ok(())
 }
