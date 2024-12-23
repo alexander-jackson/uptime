ce5bc4b4474cd59febbd2737fa1055f5358e36b7
Alexander Jackson <alexanderjackson@protonmail.com>
2024-12-23 11:17:57 +0000

    feat: add tests for query failures
    If the URL provided is nonsense, then queries will fail and be recorded
    with a reason in `query_failure`. We should make sure that this
    behaviour works before extending it for alert routing.
    
    This change:
    * Adds a test to make sure data gets stored properly

--- src/poller/tests.rs
+++ src/poller/tests.rs
@@ -3,18 +3,30 @@ use reqwest::Client;
 use sqlx::PgPool;
 use uuid::Uuid;
 
-use crate::poller::Poller;
+use crate::poller::{FailureReason, Poller};
 
 async fn fetch_latest_query_status(pool: &PgPool, uri: &str) -> Result<Option<u16>> {
     let successes =
         crate::persistence::fetch_origins_with_most_recent_success_metrics(&pool).await?;
 
     let status = successes
-        .iter()
+        .into_iter()
         .filter_map(|r| (r.uri == uri).then_some(r.status as u16))
         .next();
 
     Ok(status)
+}
+
+async fn fetch_latest_query_failure(pool: &PgPool, uri: &str) -> Result<Option<String>> {
+    let failures =
+        crate::persistence::fetch_origins_with_most_recent_failure_metrics(&pool).await?;
+
+    let failure_reason = failures
+        .into_iter()
+        .filter_map(|r| (r.uri == uri).then_some(r.failure_reason))
+        .next();
+
+    Ok(failure_reason)
 }
 
 #[sqlx::test]
@@ -69,6 +81,29 @@ async fn can_record_client_failures(pool: PgPool) -> Result<()> {
     let status = fetch_latest_query_status(&pool, &uri).await?;
 
     assert_eq!(status, Some(404));
+
+    Ok(())
+}
+
+#[sqlx::test]
+async fn can_record_query_failures(pool: PgPool) -> Result<()> {
+    // intentionally invalid TLD
+    let uri = "https://mozilla.rust";
+
+    let client = Client::new();
+    let poller = Poller::new(pool.clone(), client);
+
+    let origin_uid = Uuid::new_v4();
+    crate::persistence::insert_origin(&pool, origin_uid, uri).await?;
+
+    poller.query_all_origins().await?;
+
+    let failure_reason = fetch_latest_query_failure(&pool, uri).await?;
+
+    assert_eq!(
+        failure_reason.as_deref(),
+        Some(FailureReason::BadRequest.as_str())
+    );
 
     Ok(())
 }
