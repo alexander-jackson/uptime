30c555e3cf98b20ed3681036e44cca1ca3d66a3f
Alexander Jackson <alexanderjackson@protonmail.com>
2024-12-23 11:09:19 +0000

    feat: add tests for client failures (#17)
    When the server responds with a 4XX, this implies a failure on the
    client's part and thus is a successful request. Let's add a test to
    ensure these get recorded correctly by the poller.
    
    This change:
    * Adds a test for handling 404s

--- src/poller/tests.rs
+++ src/poller/tests.rs
@@ -1,11 +1,21 @@
-use std::collections::HashMap;
-
 use color_eyre::eyre::Result;
 use reqwest::Client;
 use sqlx::PgPool;
 use uuid::Uuid;
 
 use crate::poller::Poller;
+
+async fn fetch_latest_query_status(pool: &PgPool, uri: &str) -> Result<Option<u16>> {
+    let successes =
+        crate::persistence::fetch_origins_with_most_recent_success_metrics(&pool).await?;
+
+    let status = successes
+        .iter()
+        .filter_map(|r| (r.uri == uri).then_some(r.status as u16))
+        .next();
+
+    Ok(status)
+}
 
 #[sqlx::test]
 async fn can_query_all_origins(pool: PgPool) -> Result<()> {
@@ -28,14 +38,37 @@ async fn can_query_all_origins(pool: PgPool) -> Result<()> {
 
     mock.assert_async().await;
 
-    let origins: HashMap<String, i16> =
-        crate::persistence::fetch_origins_with_most_recent_success_metrics(&pool)
-            .await?
-            .into_iter()
-            .map(|o| (o.uri, o.status))
-            .collect();
+    let status = fetch_latest_query_status(&pool, &uri).await?;
 
-    assert_eq!(origins.get(&uri).copied(), Some(200));
+    assert_eq!(status, Some(200));
+
+    Ok(())
+}
+
+#[sqlx::test]
+async fn can_record_client_failures(pool: PgPool) -> Result<()> {
+    let mut server = mockito::Server::new_async().await;
+    let uri = server.url();
+
+    let client = Client::new();
+    let poller = Poller::new(pool.clone(), client);
+
+    let origin_uid = Uuid::new_v4();
+    crate::persistence::insert_origin(&pool, origin_uid, &uri).await?;
+
+    let mock = server
+        .mock("GET", "/")
+        .with_status(404)
+        .create_async()
+        .await;
+
+    poller.query_all_origins().await?;
+
+    mock.assert_async().await;
+
+    let status = fetch_latest_query_status(&pool, &uri).await?;
+
+    assert_eq!(status, Some(404));
 
     Ok(())
 }
